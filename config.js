adf43486becf4c6e9cba9868a1e2b4a9b18b7f41
Gunjan Srivastava <Techteam@ringdoctors.com>
2020-09-27 20:06:49 -0700

    Added javascipt file to post tweet and the configuration file

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:         'q3jlBIfKjejp2EXaD9l86Okin',
+    consumer_secret:      'SKi9UnV6yFbzRBl09cFWloeqPaMZKoJOG51Y9ypWblDSvmYEQ3',
+    access_token:         '1306397020106547201-WdojVtA2rgMZX3LddNu4kddHhuvzU5',
+    access_token_secret:  'j0qs9hf8viCfCVk30eswKwc8hRHMYhkhEWsQNl87UIgr5'
+}
