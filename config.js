06f4932a746c02b000cac040c8d06cf7cfa7b240
Anupama-sjsu <70232769+Anupama-sjsu@users.noreply.github.com>
2020-09-30 16:18:11 -0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 module.exports = {
-    consumer_key:         'q3jlBIfKjejp2EXaD9l86Okin',
-    consumer_secret:      'SKi9UnV6yFbzRBl09cFWloeqPaMZKoJOG51Y9ypWblDSvmYEQ3',
-    access_token:         '1306397020106547201-CEdWK9PVEYv9gpctatyJcsqi5HbELY',
-    access_token_secret:  'a8NBnVNgcXk6n2PA0l4G50dsw9QYJ9uJRfQ8goek9DNr3'
+	credentials: {
+    consumer_key:         'uObuKyZkbdYfqXhSqbR5JTWrP',
+    consumer_secret:      'BVsdTNoM49BHE2fQbfNQ63BudyhLZPWAYB1iZpE29k2RKvMjYn',
+    access_token:         '1309637814082924544-0ETnFT6Q6uGFUuDWJcKjpbr1YJIHTQ',
+    access_token_secret:  'M4BCT9HFFFubD03wbVrtGoQlGdmUNR2gKxXz6g3WZ9U5r'
+  },
+  limit: 300,
+  limitDays: 7
+	
 }
---
+++ index.js
@@ -0,0 +1,18 @@
+
+var Twit = require('twit');
+var opts = require('./config');
+const fetchTimeline = require('fetch-timeline');
+var container = document.getElementById('container');
+ 
+const params = {
+  screenName: 'GunjanS40215131'
+}
+
+const stream = fetchTimeline(params, opts) // => Readable Stream
+
+
+stream.on('data', (tweet, index) => {
+  console.log(`${tweet.text}`)
+  container.innerHTML = '${tweet.text}';
+})
+
